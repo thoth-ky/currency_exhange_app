234f83de710d61fafe8978798f02db2fdfe34e41
Thoth Ky <28805113+thoth-ky@users.noreply.github.com>
2021-05-10 17:19:56 +0300

    ch: correct gunicorn command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py collectstatic --no-input
 python manage.py migrate
 # collect static files
-gunicorn --worker-class gevent -w 4 -b "0.0.0.0:8000" whistle.wsgi --log-level DEBUG
+gunicorn --worker-class gevent -w 4 -b "0.0.0.0:8000" currency_exchange_app.wsgi --log-level DEBUG
