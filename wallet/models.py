a7c60318c2f11c747c9c52d77e8cb48c03f46bbd
Thoth Ky <28805113+thoth-ky@users.noreply.github.com>
2021-05-10 09:32:04 +0300

    fx: change balance field into a normal decimal field

--- wallet/models.py
+++ wallet/models.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-from djmoney.models.fields import MoneyField
+from djmoney.settings import CURRENCY_CHOICES
+from djmoney.settings import DEFAULT_CURRENCY_CODE
 
 from wallet.utils.currencies import list_currency_choices
 
 
-CURRENCY_CHOICES = list_currency_choices()
+# CURRENCY_CHOICES = list_currency_choices()
 
 
 def get_sentinel_user():
@@ -17,17 +18,16 @@ UserModel = get_user_model()
 class Wallet(models.Model):
     profile_pic = models.ImageField(upload_to="profiles/", null=True, blank=True)
     default_currency = models.CharField(
-        max_length=3, choices=CURRENCY_CHOICES, blank=False, null=False
+        max_length=3, choices=CURRENCY_CHOICES, default=DEFAULT_CURRENCY_CODE
     )
     user = models.ForeignKey(UserModel, on_delete=models.SET(get_sentinel_user))
-    balance = MoneyField(
-        max_digits=19, decimal_places=4, null=True, default_currency=None
-    )
+    balance = models.DecimalField(max_digits=19, decimal_places=4, default=0)
 
     def __str__(self):
         return f"{self.user.username} - {self.default_currency}"
 
     def credit(self, amount):
+        # amount is a DJMOney MOneyField, so it has a currency attribute and a amount attribute
         pass
 
     def debit(self, amount):
